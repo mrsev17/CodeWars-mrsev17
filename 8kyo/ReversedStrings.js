dcc5905aa36ef02b0149e99fc2f4a0af30419e26
mrsev17 <mr.upgrade3@protonmail.com>
2023-12-16 04:55:59 +0200

    Added solution for kata Reversed Strings

---
+++ 8kyo/ReversedStrings.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/5168bb5dfe9a00b126000018/train/javascript
+
+'use strict';
+
+const solution = (str) => str.split('').reverse().join('');
+
+console.log(solution('Hello, World'));
