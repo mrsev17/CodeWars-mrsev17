0cecc4e8cbf1fce97cbea2ced7333844eefd8c21
mrsev17 <mr.upgrade3@protonmail.com>
2023-12-17 14:06:20 +0200

    Added solution for kata Remove first and Last character

---
+++ 8kyo/RemoveFirstAndLastCharacter.js
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0/train/javascript
+
+'use strict';
+
+const removeChar = (str) => {
+    const strArr = str.split('');
+    strArr.shift();
+    strArr.pop();
+    return strArr.join('');
+};
+
+console.log(removeChar('Hello'));
